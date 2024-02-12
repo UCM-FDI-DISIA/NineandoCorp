f6f615a457bd204e9208e308744f128b24402c30
DavidRubioMoreno <darubi04@ucm.es>
2024-02-12 16:52:24 +0100

    towerattackprogreso

--- ProyectoH/TowerAttackComponent.h
+++ ProyectoH/TowerAttackComponent.h
@@ -1,5 +1,14 @@
 #pragma once
 
-class TowerAttackcomponent {
+#include "src/ecs/Component.h"
+
+class TowerAttackcomponent : public Component
+{
+	float reload;
+	float range;
+	virtual void shoot();
+public:
+	TowerAttackcomponent();
+	virtual void update() override;
 
 };
