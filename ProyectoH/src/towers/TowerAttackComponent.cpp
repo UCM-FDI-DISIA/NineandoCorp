5ea74689b93f57cf1b99ec5a4b36abf337ff81a5
DavidRubioMoreno <darubi04@ucm.es>
2024-02-12 18:44:23 +0100

    towerAttackComponent progreso

--- ProyectoH/src/towers/TowerAttackComponent.cpp
+++ ProyectoH/src/towers/TowerAttackComponent.cpp
@@ -1,6 +1,43 @@
 #include "TowerAttackComponent.h"
+#include "SDL.h"
+#include "algorithm"
 
-TowerAttackComponent::TowerAttackComponent() {
-	
+TowerAttackComponent::TowerAttackComponent(float range, float reloadTime, int damage) : range(range), reloadTime(reloadTime), damage(damage){
+	enemy = nullptr; elapsedTime = 0; timeToShoot = reloadTime; 
 }
+
+void TowerAttackComponent::update() {
+	elapsedTime = timer.currTime();
+	if (elapsedTime > timeToShoot) {
+		timeToShoot += reloadTime;
+		shoot();//Dispara si esta recargado
+	}
+	targetEnemy();
+}
+
+void TowerAttackComponent::targetEnemy() {
+	//if (enemy == nullptr) {//Si no hay enemigo targeteado se busca uno
+	//	float closestEnemy = 100000;
+	//	Entity* target = nullptr;
+	//	for (auto enemy : mngr_->enemigos)
+	//	{
+	//		float distance = getDistance(enemy->getComponent<Transform>().getPosition());
+	//		if(distance < range && distance < closestEnemy){
+	//			target = enemy;
+	//			closestEnemy = distance;
+	//		}
+	//	}		
+	//}	
+	//else {
+	//	if (getDistance(enemy->getComponent<Transform>().getPosition()) > range) {
+	//		enemy = nullptr;
+	//	}
+	//}
+}
+
+float TowerAttackComponent::getDistance(Vector2D enemyPos) {
+	Vector2D myPos = { 0,0 };//ent_->getComponent<Transform>().getPosition();
+	return sqrt(pow(myPos.getX() - enemyPos.getX(), 2) + pow(myPos.getY() - enemyPos.getY(), 2));
+}
+
 
--- ProyectoH/src/towers/TowerAttackComponent.h
+++ ProyectoH/src/towers/TowerAttackComponent.h
@@ -1,14 +1,24 @@
 #pragma once
 #include "..//ecs/Component.h"
+#include "..//sdlutils/VirtualTimer.h"
+#include "..//utils/Vector2D.h"
 
+class Entity;
 class TowerAttackComponent : public Component
 {
 public:
-	TowerAttackComponent();
+	TowerAttackComponent(float range, float reloadTime, int damage);
 	virtual void update() override;
 private:
-	virtual void shoot();
+	virtual void shoot() {};
+	float getDistance(Vector2D enemyPos);
+	void targetEnemy();
+	float elapsedTime;
 	float range;
 	float reloadTime;
+	float timeToShoot;
+	int damage;
+	Entity* enemy;
+	VirtualTimer timer;
 };
 
