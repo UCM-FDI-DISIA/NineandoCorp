4210abbb5df20b5fb8e714ace3319c8f76a812a2
SergioPerezRobledano <sepere12@ucm.es>
2024-02-14 17:14:08 +0100

    MovementComponent y RouteComponent

---
+++ ProyectoH/src/components/RouteComponent.cpp
@@ -0,0 +1,21 @@
+#include "RouteComponent.h"
+void
+RouteComponent::update() {
+	checkdestiny();
+}
+void
+RouteComponent::initComponent() {
+	transform_ = mngr_->getComponent<Transform>(ent_);
+	changevelocity(route_[0]);
+}
+void
+RouteComponent::checkdestiny() {
+	if (transform_->getPosition() == &route_[destiny_]) {
+		destiny_++;
+		changevelocity(route_[destiny_]);
+	}
+}
+void
+RouteComponent::changevelocity(Vector2D destino) {
+	transform_->setVelocity(destino - transform_->getPosition());
+}
