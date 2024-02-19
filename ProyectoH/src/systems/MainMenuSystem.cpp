66ce637052503a0e7df6ff67dd3ddc5ec67ad456
Vivu24 <javivu2.12004@gmail.com>
2024-02-19 16:03:19 +0100

    Comienzo MainMenuSystem

--- ProyectoH/src/systems/MainMenuSystem.cpp
+++ ProyectoH/src/systems/MainMenuSystem.cpp
@@ -1,1 +1,28 @@
 #include "MainMenuSystem.h"
+#include "../ecs/Manager.h"
+
+MainMenuSystem::MainMenuSystem() {
+
+}
+
+MainMenuSystem::~MainMenuSystem() {
+
+}
+
+void MainMenuSystem::initSystem() {
+
+}
+
+void MainMenuSystem::receive(const Message& m) {
+	/*switch (m.id)
+	{
+		case:
+			break;
+		default:
+			break;
+	}*/
+}
+
+void MainMenuSystem::update() {
+
+}
--- ProyectoH/src/systems/MainMenuSystem.h
+++ ProyectoH/src/systems/MainMenuSystem.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "..//ecs/System.h"
+//#include "../systems/RenderSystem.h"
+
 class MainMenuSystem
 {
+public:
+	static constexpr sysId_type id = _sys_MAINMENU;
+	MainMenuSystem();
+	virtual ~MainMenuSystem();
+
+	void initSystem() override;
+	void receive(const Message& m) override;
+	void update();
 };
 
