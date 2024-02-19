b3093c0286515687f220e47d06d93a326fc30732
Adriaan63 <Adcast07@ucm.es>
2024-02-19 15:43:53 +0100

    transform de los tile

--- ProyectoH/src/systems/mapSystem.cpp
+++ ProyectoH/src/systems/mapSystem.cpp
@@ -1,14 +1,14 @@
 #include "mapSystem.h"
 
-mapSystem::mapSystem(std::string filename){
-    loadMap(filename);
+mapSystem::mapSystem(std::string filename): filename(filename), winner_(0){
+    
 }
 
 mapSystem::~mapSystem() {
 }
 
 void mapSystem::initSystem(){
-    
+	loadMap(filename);
 }
 
 void mapSystem::receive(const Message& m) {
@@ -44,31 +44,53 @@ void mapSystem::loadMap(std::string filename) {
 void mapSystem::loadTile(const tmx::Map& map, const tmx::TileLayer& layer){
 	const auto& tileSets = map.getTilesets();
 	const auto& layerIDs = layer.getTiles();
-
+	int i = 0;
+	int j = 0;
 	for (auto tile : layerIDs)
 	{
 		if (tile.ID == 2) {
 			Entity* tile = mngr_->addEntity(_grp_TILES_L2);
+			Vector2D tilePosition((layer.getOffset().x + i),
+				(layer.getOffset().y + j));
+			mngr_->addComponent<Transform>(tile)->setPosition(tilePosition);
 			mngr_->addComponent<RenderComponent>(tile, gameTextures::hillTexture);
+			
 		}
 		else if (tile.ID == 4) {
 			Entity* tile = mngr_->addEntity(_grp_TILES_L3);
+			Vector2D tilePosition((layer.getOffset().x + i),
+				(layer.getOffset().y + j));
+			mngr_->addComponent<Transform>(tile)->setPosition(tilePosition);
 			mngr_->addComponent<RenderComponent>(tile, gameTextures::mountainTexture);
 		}
 		else if (tile.ID == 133) {
 			Entity* tile = mngr_->addEntity(_grp_TILES_L2);
+			Vector2D tilePosition((layer.getOffset().x + i),
+				(layer.getOffset().y + j));
+			mngr_->addComponent<Transform>(tile)->setPosition(tilePosition);
 			mngr_->addComponent<RenderComponent>(tile, gameTextures::roadTexture);
 		}
 		else if (tile.ID == 85) {
 			Entity* tile = mngr_->addEntity(_grp_TILES_L1);
+			Vector2D tilePosition((layer.getOffset().x + i),
+				(layer.getOffset().y + j));
+			mngr_->addComponent<Transform>(tile)->setPosition(tilePosition);
 			mngr_->addComponent<RenderComponent>(tile, gameTextures::lakeTexture);
 		}
 		//Hay que dividir cada tile del borde a su respectiva textura (mas if)
 		else if (tile.ID > 80 && tile.ID < 101) {
 			Entity* tile = mngr_->addEntity(_grp_TILES_L1);
+			Vector2D tilePosition((layer.getOffset().x + i),
+				(layer.getOffset().y + j));
+			mngr_->addComponent<Transform>(tile)->setPosition(tilePosition);
 			mngr_->addComponent<RenderComponent>(tile, gameTextures::lakeEdgeTexture);
 		}
-
+		i++;
+		if (i % 32 == 0)
+		{
+			i = 0;
+			j++;
+		}
 	}
 }
 
