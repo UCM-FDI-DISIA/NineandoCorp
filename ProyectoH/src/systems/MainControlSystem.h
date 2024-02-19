839472ed1c68a9615968ca7755546c984d157d35
JaviBravoPerucho <jabrav03@ucm.es>
2024-02-19 16:47:05 +0100

    MainControlSystem
    Se encarga de gestionar las monedas doradas de la partida y el numero de oleada

--- ProyectoH/src/systems/MainControlSystem.h
+++ ProyectoH/src/systems/MainControlSystem.h
@@ -1,25 +1,35 @@
 #pragma once
 #include "../ecs/System.h"
+#include "..//sdlutils/VirtualTimer.h"
+#include "../game/Game.h"
 
 class MainControlSystem : public System
 {
 public:
 	static constexpr sysId_type id = _sys_MAINCONTROL;
 
-	MainControlSystem(int doradasIniciales);
-	/*~MainControlSystem();
+	MainControlSystem(int doradasIniciales, int doradasPorSegundo, int numOleadas, float tiempoEntreOleadas);
+	~MainControlSystem() {};
 
-	void initSystem() override;
+	void initSystem() override {};
 	void receive(const Message& m) override;
 	void update();
 	void onRoundOver();
-	void onRoundStart();*/
+	void onRoundStart();
+	void subtractCoins(int num);
 
+	int getOleadaActual() { return oleadaActual; }
+	int getDoradasActuales() { return numDoradasActuales; }
+	float getTiempoEntreOleadas() { return tiempoEntreOleadas; }
 protected:
 	bool active_;
 	int numDoradasActuales;
 	int numDoradasIniciales;
 	int numDoradasPorSegundo;
 	int oleadaActual;
+	int numOleadas;
+	float tiempoEntreOleadas;
+	double elapsedTime_;
+	VirtualTimer timer_;
 };
 
