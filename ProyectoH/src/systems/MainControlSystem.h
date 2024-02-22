1e62fde17a0b771b87836f09097ea1c195c05a1e
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2024-02-22 16:33:14 +0100

    UML update

--- ProyectoH/src/systems/MainControlSystem.h
+++ ProyectoH/src/systems/MainControlSystem.h
@@ -14,13 +14,7 @@ public:
 	void initSystem() override {};
 	void receive(const Message& m) override;
 	void update();
-	void onRoundOver();
-	void onRoundStart();
-	void subtractCoins(int num);
 
-	int getOleadaActual() { return oleadaActual; }
-	int getDoradasActuales() { return numDoradasActuales; }
-	float getTiempoEntreOleadas() { return tiempoEntreOleadas; }
 protected:
 	bool active_;
 	int numDoradasActuales;
@@ -31,5 +25,9 @@ protected:
 	float tiempoEntreOleadas;
 	double elapsedTime_;
 	VirtualTimer timer_;
+
+	void onRoundOver();
+	void onRoundStart();
+	void subtractCoins(int num);
 };
 
