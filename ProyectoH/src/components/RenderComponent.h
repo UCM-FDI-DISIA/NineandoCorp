fd426744bd073530666d8cc7fcd2efb655ebd9e9
damore11 <damore11@ucm.es>
2024-02-26 15:54:19 +0100

    sprites de torres

--- ProyectoH/src/components/RenderComponent.h
+++ ProyectoH/src/components/RenderComponent.h
@@ -4,9 +4,8 @@
 
 // Correspondant texture to each type of entity
 enum gameTextures {
-	tileSet, hillTexture, roadTexture, mountainTexture, lakeTexture1, lakeTexture2, lakeTexture3, lakeTexture4,
-	lakeTexture5, lakeTexture6, lakeTexture7, lakeTexture8, lakeTexture9, lakeTexture10,
-	lakeTexture11, bulletTexture, play, playHover,square, bulletTowerTexture,gmTxtrSize
+	tileSet, bulletTexture, play, playHover,square, bulletTowerTexture, cristalTowerTexture, phoenixTowerTexture,
+	slimeTowerTexture, boosterTowerTexture, sniperTowerTexture, clayTowerTexture,gmTxtrSize
 };
 class RenderComponent : public Component
 {
