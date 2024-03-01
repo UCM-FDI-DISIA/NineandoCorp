29c584360cf72780dc232337ed4d7d666a06e877
Adriaan63 <Adcast07@ucm.es>
2024-03-01 09:55:59 +0100

    cambios en los enemigos
    hemos añadido 2 enemigos nuevos de rango que sustituiran a 2 mele y hay 4 jefes nuevos

--- ProyectoH/src/systems/mapSystem.h
+++ ProyectoH/src/systems/mapSystem.h
@@ -26,6 +26,16 @@
 
 class mapSystem : public System {
 private:
+	// Para gestionar los mensajes correspondientes y actualizar los atributos
+	// winner_ y state_.
+	void onRoundStart();
+	void onGameStart();
+	void onGameOver(Uint8 winner);
+	// Displays pause message
+	void onPause();
+	// Hides pause message
+	void onResume();
+
 	uint8_t winner_;
 	tmx::Vector2f m_chunkSize = tmx::Vector2f(64.f, 64.f);
 	tmx::Vector2u m_MapTileSize;
