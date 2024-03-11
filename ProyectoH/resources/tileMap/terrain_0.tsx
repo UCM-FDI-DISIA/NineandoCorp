3faa0fe2897b850160f9a43d844c4379cc63d532
Adriaan63 <Adcast07@ucm.es>
2024-03-11 15:45:01 +0100

    la lie con el tilesheet
    lo subo para que nico pueda trabajar

--- ProyectoH/resources/tileMap/terrain_0.tsx
+++ ProyectoH/resources/tileMap/terrain_0.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="terrain_0" tilewidth="64" tileheight="64" tilecount="160" columns="10">
- <image source="../../../../../../../Downloads/terrain_0.png" width="640" height="1024"/>
+ <image source="../images/Map/terrain_0.png" width="640" height="1024"/>
 </tileset>
