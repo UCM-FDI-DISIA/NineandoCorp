822befba387c66f3557fd041ce2bd4b38296c566
Mikeey84 <milope21@ucm.es>
2024-04-08 16:55:36 +0200

    se mueve con wasd

--- ProyectoH/src/systems/RenderSystem.cpp
+++ ProyectoH/src/systems/RenderSystem.cpp
@@ -180,28 +180,28 @@ void RenderSystem::update() {
 
 		//Este control tiene que estar en el main control sistem
 		////Control de camara
-		if (ih().isKeyDown(SDLK_UP)) {
+		if (ih().isKeyDown(SDL_SCANCODE_W)) {
 			k_up = true;
 		}
-		else if (ih().isKeyUp(SDLK_UP)) {
+		else if (ih().isKeyUp(SDL_SCANCODE_W)) {
 			k_up = false;
 		}
-		if (ih().isKeyDown(SDLK_LEFT)) {
+		if (ih().isKeyDown(SDL_SCANCODE_A)) {
 			k_left = true;
 		}
-		else if (ih().isKeyUp(SDLK_LEFT)) {
+		else if (ih().isKeyUp(SDL_SCANCODE_A)) {
 			k_left = false;
 		}
-		if (ih().isKeyDown(SDLK_RIGHT)) {
+		if (ih().isKeyDown(SDL_SCANCODE_D)) {
 			k_right = true;
 		}
-		else if (ih().isKeyUp(SDLK_RIGHT)) {
+		else if (ih().isKeyUp(SDL_SCANCODE_D)) {
 			k_right = false;
 		}
-		if (ih().isKeyDown(SDLK_DOWN)) {
+		if (ih().isKeyDown(SDL_SCANCODE_S)) {
 			k_down = true;
 		}
-		else if (ih().isKeyUp(SDLK_DOWN)) {
+		else if (ih().isKeyUp(SDL_SCANCODE_S)) {
 			k_down = false;
 		}
 
