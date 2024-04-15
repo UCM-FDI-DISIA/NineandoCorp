d9d1974c96bc5714be6cc3f3591ca7ec45689576
DavidRubioMoreno <darubi04@ucm.es>
2024-04-15 15:46:44 +0200

    mejoras torres

--- ProyectoH/src/components/UpgradeTowerComponent.cpp
+++ ProyectoH/src/components/UpgradeTowerComponent.cpp
@@ -27,28 +27,121 @@ void UpgradeTowerComponent::levelUp() {
 		currentLevel_++;
 	}
 	if (currentLevel_ == 0) { currentLevel_ = 1; }
+
+	auto bt = mngr_->getComponent<BulletTower>(ent_);
+	auto ct = mngr_->getComponent<CrystalTower>(ent_);
+	auto st = mngr_->getComponent<SlimeTowerComponent>(ent_);
+	auto ds = mngr_->getComponent<DiegoSniperTower>(ent_);
+	auto ft = mngr_->getComponent<PhoenixTower>(ent_);
+	auto pt = mngr_->getComponent<EnhancerTower>(ent_);
+	auto dt = mngr_->getComponent<DirtTower>(ent_);
+	auto h = mngr_->getComponent<HealthComponent>(ent_);
 	
 	switch (id_) {
-		case _twr_BULLET:
-			mngr_->getComponent<BulletTower>(ent_)->levelUp(currentLevel_);
+		case _twr_BULLET:			
+			switch (currentLevel_) {
+			case 1:
+				bt->setDamage(sdlutils().intConst().at("BalasDano1"));
+				break;
+			case 2:
+				bt->setDamage(sdlutils().intConst().at("BalasDano2"));
+				break;
+			case 3:
+				bt->setDamage(sdlutils().intConst().at("BalasDano3"));
+				break;
+			case 4:
+				break;
+			default:
+				break;
+			}
 			break;
-		case _twr_CRISTAL:
-			mngr_->getComponent<CrystalTower>(ent_)->levelUp(currentLevel_);
+		case _twr_CRISTAL:			
+			ct->setShieldVal(ct->getShieldVal() + sdlutils().floatConst().at("CristalEscudo1"));
 			break;
 		case _twr_SLIME:
-			mngr_->getComponent<SlimeTowerComponent>(ent_)->levelUp(currentLevel_);
+			
+			switch (currentLevel_) {
+			case 1:
+				st->setTimeToShoot(st->getTimeToShoot() - sdlutils().floatConst().at("SlimeRecargaUpdate"));
+				break;
+			case 2:
+				st->setSpeedDecrease(sdlutils().floatConst().at("SlimeRalentizacion"));
+				break;
+			case 3:
+				st->setSlimeDuration(st->getDuration() - sdlutils().floatConst().at("SlimeTiempoSlime"));
+				break;
+			case 4:
+				break;
+			default:
+				break;
+			}
 			break;
 		case _twr_DIEGO:
-			mngr_->getComponent<DiegoSniperTower>(ent_)->levelUp(currentLevel_);
+			
+			switch (currentLevel_) {
+			case 1:
+				ds->setDamage(sdlutils().floatConst().at("DiegoSniperDano1"));
+				break;
+			case 2:
+				ds->setCritDamage(sdlutils().floatConst().at("DiegoSniperCritDano1"));
+				ds->setCritProb(sdlutils().floatConst().at("DiegoSniperCritProb1"));
+				break;
+			case 3:
+				ds->setTimeToShoot(ds->getTimeToShoot() - sdlutils().floatConst().at("DiegoSniperRecargaUpdate1"));
+				break;
+			case 4:
+				ds->setCritProb(sdlutils().floatConst().at("DiegoSniperCritProb2"));
+				ds->setTimeToShoot(ds->getTimeToShoot() - sdlutils().floatConst().at("DiegoSniperRecargaUpdate1"));
+				break;
+			default:
+				break;
+			}
 			break;
 		case _twr_FENIX:
-			mngr_->getComponent<PhoenixTower>(ent_)->levelUp(currentLevel_);
+			
+			switch (currentLevel_) {
+			case 1:
+				ft->setDamage(sdlutils().floatConst().at("FenixDPS1"));
+				break;
+			case 2:
+				ft->setCooling(sdlutils().floatConst().at("FenixEnfriamiento1"));
+				break;
+			case 3:
+				ft->setDamage(sdlutils().floatConst().at("FenixDPS2"));
+				ft->setCooling(sdlutils().floatConst().at("FenixEnfriamiento2"));
+				break;
+			case 4:
+				ft->setCooling(sdlutils().floatConst().at("FenixEnfriamientoCero"));
+				//Falta anadir la opcion de aumentar el rango del fuego
+				break;
+			default:
+				break;
+			}	
 			break;
 		case _twr_CLAY:
-			mngr_->getComponent<DirtTower>(ent_)->levelUp(currentLevel_);
+			
+			h->setMaxHealth(sdlutils().floatConst().at("ArcillaVida" + to_string((int)currentLevel_)));
+			h->resetHealth();
 			break;
 		case _twr_POWER:
-			mngr_->getComponent<EnhancerTower>(ent_)->levelUp(currentLevel_);
+			
+			switch (currentLevel_) {
+			case 1:
+				pt->setDamageIncreasePercentage(sdlutils().floatConst().at("PotenciadoraDano1"));
+				break;
+			case 2:
+				pt->setTowersHPboost(sdlutils().floatConst().at("PotenciadoraAumentoVida"));
+				break;
+			case 3:
+				pt->setDamageIncreasePercentage(sdlutils().floatConst().at("PotenciadoraDano2"));
+				break;
+			case 4:
+				pt->setDamageIncreasePercentage(sdlutils().floatConst().at("PotenciadoraDano3"));
+				pt->setTowersHPboost(sdlutils().floatConst().at("PotenciadoraVida2"));
+				break;
+			default:
+				break;
+			}
 			break;
 		default:
 			break;
