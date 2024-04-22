41660fcb358956dd426bd7f14dff61e4e491d80a
[MarcosPRC] <marcopan@ucm.es>
2024-04-22 21:37:35 +0200

    Enemybook a mitad, falta habilidad especial y rango

--- ProyectoH/src/systems/EnemyBookSystem.cpp
+++ ProyectoH/src/systems/EnemyBookSystem.cpp
@@ -70,7 +70,8 @@ void  EnemyBookSystem::receive(const Message& m) {
 	case _m_ENEMY_BOOKPOPUP:
 		EnemyPopUp(m.start_enemy_book_but.n);
 		break;
-	default:
+
+		default:
 		break;
 	}
 }
@@ -85,44 +86,166 @@ void EnemyBookSystem::EnemyPopUp(int i) {
 	sAux = { 50.0f, 50.0f };
 	bS->addButton(pAux, sAux, gameTextures::close, gameTextures::close_hover, ButtonTypes::back_selector);
 
-	bool habilidad = false;
-	int vel = 0;
-	int vida = 0;
-	int tipo = 0;
-	int atq = 0;
+	string habilidad = "";
+	string vel = "";
+	string vida = "";
+	string tipo = "";
+	string atq = "";
+	string nombre = "";
+	string rango = "";
+	string Tiempo = "";
 	switch (i)
 	{
+		case 0:
+			nombre = "Maestro de Almas";
+			vida = std::to_string(sdlutils().intConst().at("MaestroVida"));
+			atq = std::to_string(sdlutils().intConst().at("MaestroAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("MaestroVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Maestrotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			break;
 		case 1:
-			//coger datos json
-
+			nombre = "Acechante Electrico";
+			vida = std::to_string(sdlutils().intConst().at("AcechanteVida"));
+			atq = std::to_string(sdlutils().intConst().at("AcechanteAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("AcechanteVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Acechantetiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 2:
+			nombre = "Maldito";
+			vida = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			atq = std::to_string(sdlutils().intConst().at("MalditoAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("MalditoVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Malditotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 3:
+			nombre = "Golem";
+			vida = std::to_string(sdlutils().intConst().at("GolemVida"));
+			atq = std::to_string(sdlutils().intConst().at("GolemAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("GolemVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Golemtiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			
 			break;
 		case 4:
+			nombre = "Demonio Alado";
+			vida = std::to_string(sdlutils().intConst().at("DemonioAladoVida"));
+			atq = std::to_string(sdlutils().intConst().at("DemonioAladoAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("DemonioAladoVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("DemonioAladotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 5:
+			nombre = "Goblin";
+			vida = std::to_string(sdlutils().intConst().at("GoblinVida"));
+			atq = std::to_string(sdlutils().intConst().at("GoblinAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("GoblinVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Goblintiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 6:
+			nombre = "Elfo";
+			vida = std::to_string(sdlutils().intConst().at("ElfoVida"));
+			atq = std::to_string(sdlutils().intConst().at("ElfoAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("ElfoVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Elfotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 7:
+			nombre = "Mensajero de la Muerte";
+			vida = std::to_string(sdlutils().intConst().at("MensajeroVida"));
+			atq = std::to_string(sdlutils().intConst().at("MensajeroAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("MensajeroVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Mensajerotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 8:
+			nombre = "Angel";
+			vida = std::to_string(sdlutils().intConst().at("AngelVida"));
+			atq = std::to_string(sdlutils().intConst().at("AngelAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("AngelVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Angeltiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 9:
+			nombre = "Demonio Infernal";
+			vida = std::to_string(sdlutils().intConst().at("DemonioInfernalVida"));
+			atq = std::to_string(sdlutils().intConst().at("DemonioInfernalAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("DemonioInfernalVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("DemonioInfernaltiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 10:
+			nombre = "Defensor";
+			vida = std::to_string(sdlutils().intConst().at("DefensorVida"));
+			atq = std::to_string(sdlutils().intConst().at("DefensorAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("DefensorVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Defensortiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 11:
+			nombre = "C.Maldito";
+			vida = std::to_string(sdlutils().intConst().at("CMalditoVida"));
+			atq = std::to_string(sdlutils().intConst().at("CMalditoAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("CMalditoVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("CMalditotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 12:
+			nombre = "Principito";
+			vida = std::to_string(sdlutils().intConst().at("PrincipitoVida"));
+			atq = std::to_string(sdlutils().intConst().at("PrincipitoAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("PrincipitoVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Principitotiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 13:
+			nombre = "Monje";
+			vida = std::to_string(sdlutils().intConst().at("MonjeVida"));
+			atq = std::to_string(sdlutils().intConst().at("MonjeAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("MonjeVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Monjetiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		case 14:
-			break;
-		case 15:
+			nombre = "Muerte";
+			vida = std::to_string(sdlutils().intConst().at("MuerteVida"));
+			atq = std::to_string(sdlutils().intConst().at("MuerteAtaque"));
+			vel = std::to_string(sdlutils().intConst().at("MuerteVelocidad"));
+			Tiempo = std::to_string(sdlutils().intConst().at("Muertetiempo"));
+			//tipo = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//rango = std::to_string(sdlutils().intConst().at("MalditoVida"));
+			//habilidad = std::to_string(sdlutils().intConst().at("MalditoVida"));
 			break;
 		default:
 		break;
@@ -133,11 +256,30 @@ void EnemyBookSystem::EnemyPopUp(int i) {
 	bS->addImage(pAux, sAux + Vector2D{50,50}, 0.0, gameTextures::large_box, _grp_HUD_FOREGROUND);
 	bS->addImage(pAux, sAux,0.0,Texturas[i], _grp_HUD_FOREGROUND);
 	//AÑADIR NOMBRE
-	string nombre = "caballero";
 	pAux = Vector2D{ 700,420 };
-	sAux = Vector2D{ 120,100};
+	sAux = Vector2D{ 210,70};
 	SDL_Color NombreColor = { 255, 255, 255, 255 };
-	bS->addText(nombre,NombreColor,pAux,sAux);
-		//bS->addText(std::to_string(sdlutils().intConst().at("BalasPrecio")), moneyColor, { xAux * ((int)_twr_BULLET + 1) - 20 , heightH + 67 }, { bSize.getX() / 2 , 30 });
+	bS->addText(nombre, NombreColor, pAux, sAux);
+	//AÑADIR ATAQUE
+	pAux = Vector2D{ 650,470 };
+	sAux = Vector2D{ 90,30 };
+	bS->addText("Ataque: " + atq, NombreColor, pAux, sAux);
+	//AÑADIR VIDA
+	pAux = Vector2D{ 800,470 };
+	bS->addText("Vida: " +vida, NombreColor, pAux, sAux);
+	//AÑADIR VELOCIDAD
+	pAux = Vector2D{ 650,510 };
+	bS->addText("Velocidad: " +vel, NombreColor, pAux, sAux);
+	//AÑADIR TIEMPO ATAQUE
+	sAux = Vector2D{ 100,34 };
+	pAux = Vector2D{ 800,510};
+	bS->addText("Vel.Ataque: " +Tiempo, NombreColor, pAux, sAux);
+
+	//AÑADIR RANGO
+	//pAux = Vector2D{ 700,420 };
+	//bS->addText(, NombreColor, pAux, sAux);
+	//AÑADIR HABILIDAD
+	//pAux = Vector2D{ 700,420 };
+	//bS->addText(nombre, NombreColor, pAux, sAux);
 
 }
