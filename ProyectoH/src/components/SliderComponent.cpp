1f8d8336c9f04b753c2b08c1b6366faff30521ba
Nicolás Besteiro <nicobest@ucm.es>
2024-04-24 16:33:17 +0200

    pequeños arreglos

--- ProyectoH/src/components/SliderComponent.cpp
+++ ProyectoH/src/components/SliderComponent.cpp
@@ -1,10 +1,13 @@
 #include "SliderComponent.h"
 #include "../ecs/Manager.h"
+#include "../game/Game.h"
 
 void SliderComponent::initComponent() {
 	tr_ = mngr_->getComponent<Transform>(ent_);
 }
 bool SliderComponent::isPressed(const Vector2D& mousePos) {
+	sdlutils().soundEffects().at("button").setChannelVolume(game().CalculoVolumenEfectos(), 1);
+	sdlutils().soundEffects().at("button").play(0, 1);
 	auto pos = tr_->getPosition();
 	auto height = tr_->getHeight();
 	auto width = tr_->getWidth();
