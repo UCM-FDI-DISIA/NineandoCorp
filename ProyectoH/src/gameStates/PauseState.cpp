18fdb61f525dac2c17db07ffb89303c2c61b5bfc
Adriaan63 <Adcast07@ucm.es>
2024-05-20 11:25:46 +0200

    Update PauseState.cpp

--- ProyectoH/src/gameStates/PauseState.cpp
+++ ProyectoH/src/gameStates/PauseState.cpp
@@ -7,5 +7,6 @@ PauseState::PauseState(Manager* mngr) : GameState(_gmStt_PAUSE, mngr) {
 PauseState::~PauseState() {
 	mngr_->removeSystem<PauseSystem>(); 
 	mngr_->getSystem<ButtonSystem>()->changeButtonsIdHandler(_hdlr_BUTTON_PLAY);
+	mngr_->getSystem<RenderSystem>()->initSystem();
 	mngr_->addSystem<HUDSystem>();
 }
